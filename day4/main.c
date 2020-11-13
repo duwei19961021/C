84ebdf85895f77e08e29837cc32ff0eb1f470962
duwei <duwei19961021@163.com>
2020-11-13 16:32:04 +0800

    逆序排列单词

--- day4/main.c
+++ day4/main.c
@@ -13,13 +13,13 @@ void reverse_str(char * str, int start, int end){
 
 int main(){
     char str[100];
-    fgets(str,100+1,stdin);
+    fgets(str,sizeof(str-1),stdin);
     int length = strlen(str);
     str[length-1]='\0';
     length = strlen(str);
     reverse_str(str,0,length-1);
 
-    int start = 0,end = 0;
+    int start = 0,end;
     for (int i = 0; i < length+1; ++i) {
         if (' ' == str[i] || '\0' == str[i]){
             end = i-1;
