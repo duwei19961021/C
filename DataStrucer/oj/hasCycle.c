e801581fdae6dd5f9aba416bbf742685f28eda16
duwei <duwei19961021@163.com>
2020-12-27 01:25:23 +0800

    判断链表中是否有环

---
+++ DataStrucer/oj/hasCycle.c
@@ -0,0 +1,34 @@
+//
+// Created by duwei on 2020/12/27.
+//
+
+/*
+ * 给定一个链表，判断链表中是否有环。
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ * 如果链表中存在环，则返回 true 。 否则，返回 false 。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle
+ * */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+    while(fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
