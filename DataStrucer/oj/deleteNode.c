f56b9969ec92d7a3b1b4bd849d5e8efbfa98661f
duwei <duwei19961021@163.com>
2020-12-29 12:45:26 +0800

    删除链表中的结点

---
+++ DataStrucer/oj/deleteNode.c
@@ -0,0 +1,24 @@
+//
+// Created by duwei on 2020/12/29.
+//
+
+/*
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点 。
+ * 输入：head = [4,5,1,9], node = 5
+ * 输出：[4,1,9]
+ * 解释：给定你链表中值为5的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list
+ * */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+void deleteNode(struct ListNode* node) {
+    node->val = node->next->val; // 将要被删除的节点的下一个结点的值赋给要被删除的结点
+    node->next = node->next->next; // 将复制后的结点的next指向下个结点的下一个结点
+}
