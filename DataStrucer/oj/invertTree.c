1e132ee65747cea92956971a623f9b0c8b12e472
duwei <duwei19961021@163.com>
2021-01-18 20:21:53 +0800

    翻转二叉树：第二种写法

--- DataStrucer/oj/invertTree.c
+++ DataStrucer/oj/invertTree.c
@@ -60,3 +60,20 @@ struct TreeNode* invertTree(struct TreeNode* root){
     _invertTree(&newRoot);
     return newRoot;
 }
+
+
+////////////////////////////
+
+struct TreeNode* invertTree(struct TreeNode* root){
+    if(!root)
+    {
+        return NULL;
+    }
+
+    struct TreeNode* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
